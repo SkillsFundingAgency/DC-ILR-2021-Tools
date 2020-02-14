f47054ef473de1cc9917d05c2372c9530c08f08b
Sam Precious <sam.precious@nimbusdti.co.uk>
2020-02-14 13:08:05 +0000

    added file name service into the application

---
+++ src/ESFA.DC.ILR.Tools.IFCT.Service.Tests/FileNameServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace ESFA.DC.ILR.Tools.IFCT.Service.Tests
+{
+    public class FileNameServiceTests
+    {
+        [Fact]
+        public void YearUpdateValidString()
+        {
+            FileNameService service = new FileNameService();
+            var returns = service.YearUpdate("1920");
+            returns.Should().Be("2021");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("12345")]
+        [InlineData("123")]
+        [InlineData("abcd")]
+        public void YearUpdateInvalidStringParameter(string s)
+        {
+            FileNameService fileNameService = new FileNameService();
+            Action a = () => fileNameService.YearUpdate(s);
+            a.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void SerialNumberUpdateValidInput()
+        {
+            var sut = NewService();
+            string result = sut.SerialNumberUpdate("01");
+            result.Should().Be("99");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("a")]
+        [InlineData("ab")]
+        [InlineData("abc")]
+        [InlineData("1")]
+        [InlineData("123")]
+        public void SerialNumberUpdateInvalidInput(string number)
+        {
+            var sut = NewService();
+            Action a = () => sut.SerialNumberUpdate(number);
+            a.Should().Throw<ArgumentException>();
+        }
+
+
+        [Fact]
+        public void DateUpdateValidDate()
+        {
+            var sut = NewService();
+            string result = sut.DateStampUpdate("20190101");
+            result.Should().Be("20200101");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("20191301")]
+        [InlineData("20191232")]
+        [InlineData("201912320")]
+        [InlineData("2019123")]
+        [InlineData("2019Jun10")]
+        public void DateUpdateIvalidDate(string date)
+        {
+            var sut = NewService();
+            Action result = () => sut.DateStampUpdate(date);
+            result.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void NameGenerationValidInput()
+        {
+            var sut = NewService();
+            string result = sut.NameGeneration("ILR-10006341-1920-20190210-120000-01");
+            result.Should().Be("ILR-10006341-2021-20200210-120000-99");
+
+        }
+
+        private FileNameService NewService()
+        {
+            return new FileNameService();
+        }
+    }
+}
