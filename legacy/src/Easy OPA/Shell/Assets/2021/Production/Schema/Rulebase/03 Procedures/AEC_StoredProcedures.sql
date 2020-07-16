2ab8aac5aa56ad945edc17db4cc725b0dec66aa6
Sam Precious <sam.precious@nimbusdti.co.uk>
2020-07-16 14:53:12 +0000

    reverted aec

--- legacy/src/Easy OPA/Shell/Assets/2021/Production/Schema/Rulebase/03 Procedures/AEC_StoredProcedures.sql
+++ legacy/src/Easy OPA/Shell/Assets/2021/Production/Schema/Rulebase/03 Procedures/AEC_StoredProcedures.sql
@@ -81,9 +81,7 @@ select
               from
                 Reference.AEC_LatestInYearEarningHistory
               where
-                ULN = l.ULN 
-                and UKPRN = l.UKPRN
-                and [LatestInYear] = 1 for xml path ('HistoricEarningInput'),
+                ULN = l.ULN for xml path ('HistoricEarningInput'),
                 type
             ),
             (
@@ -1572,83 +1570,3 @@ UKPRN,
   [Period]
 end
 go
-
-if object_id(
-    'Rulebase.AEC_HistoricOutput_Copy',
-    'p'
-  ) is not null begin drop procedure Rulebase.AEC_HistoricOutput_Copy
-end
-go
-
-CREATE PROCEDURE Rulebase.AEC_HistoricOutput_Copy
-AS
-BEGIN
-    -- SET NOCOUNT ON added to prevent extra result sets from
-    -- interfering with SELECT statements.
-    SET NOCOUNT ON
-
-    INSERT INTO [ReferenceInput].[AppsEarningHistory_ApprenticeshipEarningsHistory]
-           ([AppIdentifier]
-           ,[AppProgCompletedInTheYearInput]
-           ,[CollectionYear]
-           ,[CollectionReturnCode]
-           ,[DaysInYear]
-           ,[FworkCode]
-           ,[HistoricEffectiveTNPStartDateInput]
-           ,[HistoricEmpIdEndWithinYear]
-           ,[HistoricEmpIdStartWithinYear]
-           ,[HistoricLearner1618StartInput]
-           ,[HistoricPMRAmount]
-           ,[HistoricTNP1Input]
-           ,[HistoricTNP2Input]
-           ,[HistoricTNP3Input]
-           ,[HistoricTNP4Input]
-           ,[HistoricTotal1618UpliftPaymentsInTheYearInput]
-           ,[HistoricVirtualTNP3EndOfTheYearInput]
-           ,[HistoricVirtualTNP4EndOfTheYearInput]
-           ,[HistoricLearnDelProgEarliestACT2DateInput]
-           ,[LatestInYear]
-           ,[LearnRefNumber]
-           ,[ProgrammeStartDateIgnorePathway]
-           ,[ProgrammeStartDateMatchPathway]
-           ,[ProgType]
-           ,[PwayCode]
-           ,[STDCode]
-           ,[TotalProgAimPaymentsInTheYear]
-           ,[UptoEndDate]
-           ,[UKPRN]
-           ,[ULN])
-     SELECT 
-		[AppIdentifierOutput]
-      ,[AppProgCompletedInTheYearOutput]
-	  ,'2021' as CollectionYear--2021
-	  ,'' as CollectionReturnCode--
-      ,[HistoricDaysInYearOutput]
-      ,[HistoricFworkCodeOutput]
-      ,[HistoricEffectiveTNPStartDateOutput]
-      ,[HistoricEmpIdEndWithinYearOutput]
-      ,[HistoricEmpIdStartWithinYearOutput]
-      ,[HistoricLearner1618AtStartOutput]
-      ,[HistoricPMRAmountOutput]
-      ,[HistoricTNP1Output]
-      ,[HistoricTNP2Output]
-      ,[HistoricTNP3Output]
-      ,[HistoricTNP4Output]
-      ,[HistoricTotal1618UpliftPaymentsInTheYear]
-      ,[HistoricVirtualTNP3EndofThisYearOutput]
-      ,[HistoricVirtualTNP4EndofThisYearOutput]
-      ,[HistoricLearnDelProgEarliestACT2DateOutput]
-	  ,1 as LatestInYear --true/1
-      ,[LearnRefNumber]
-      ,[HistoricProgrammeStartDateIgnorePathwayOutput]
-      ,[HistoricProgrammeStartDateMatchPathwayOutput]
-      ,[HistoricProgTypeOutput]
-      ,[HistoricPwayCodeOutput]
-      ,[HistoricSTDCodeOutput]
-      ,[HistoricTotalProgAimPaymentsInTheYear]
-      ,[HistoricUptoEndDateOutput]
-	  ,[UKPRN]
-      ,[HistoricULNOutput]
-  FROM [Rulebase].[AEC_HistoricEarningOutput]
-END
-GO
