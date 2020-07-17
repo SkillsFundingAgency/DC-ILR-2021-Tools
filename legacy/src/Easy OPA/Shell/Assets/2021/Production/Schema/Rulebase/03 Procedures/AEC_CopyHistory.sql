85c1b2f63e90175fb2b7d81e0d9955b1d96a3dbd
Sam Precious <sam.precious@nimbusdti.co.uk>
2020-07-17 14:38:22 +0000

    removed file and updated copy sproc

--- legacy/src/Easy OPA/Shell/Assets/2021/Production/Schema/Rulebase/03 Procedures/AEC_CopyHistory.sql
+++ legacy/src/Easy OPA/Shell/Assets/2021/Production/Schema/Rulebase/03 Procedures/AEC_CopyHistory.sql
@@ -1,193 +1,90 @@
-select
-  UnrequiredAlias.UKPRN,
-  LearnRefNumber,
-  PriceEpisodeIdentifier,
-  [Period],
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeApplic1618FrameworkUpliftBalancing' then Value
-      else null
-    end
-  ) as PriceEpisodeApplic1618FrameworkUpliftBalancing,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeApplic1618FrameworkUpliftCompletionPayment' then Value
-      else null
-    end
-  ) as PriceEpisodeApplic1618FrameworkUpliftCompletionPayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeApplic1618FrameworkUpliftOnProgPayment' then Value
-      else null
-    end
-  ) as PriceEpisodeApplic1618FrameworkUpliftOnProgPayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeBalancePayment' then Value
-      else null
-    end
-  ) as PriceEpisodeBalancePayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeBalanceValue' then Value
-      else null
-    end
-  ) as PriceEpisodeBalanceValue,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeCompletionPayment' then Value
-      else null
-    end
-  ) as PriceEpisodeCompletionPayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeFirstDisadvantagePayment' then Value
-      else null
-    end
-  ) as PriceEpisodeFirstDisadvantagePayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeFirstEmp1618Pay' then Value
-      else null
-    end
-  ) as PriceEpisodeFirstEmp1618Pay,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeFirstProv1618Pay' then Value
-      else null
-    end
-  ) as PriceEpisodeFirstProv1618Pay,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeInstalmentsThisPeriod' then Value
-      else null
-    end
-  ) as PriceEpisodeInstalmentsThisPeriod,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeLearnerAdditionalPayment' then Value
-      else null
-    end
-  ) as PriceEpisodeLearnerAdditionalPayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeLevyNonPayInd' then Value
-      else null
-    end
-  ) as PriceEpisodeLevyNonPayInd,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeLSFCash' then Value
-      else null
-    end
-  ) as PriceEpisodeLSFCash,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeOnProgPayment' then Value
-      else null
-    end
-  ) as PriceEpisodeOnProgPayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeProgFundIndMaxEmpCont' then Value
-      else null
-    end
-  ) as PriceEpisodeProgFundIndMaxEmpCont,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeProgFundIndMinCoInvest' then Value
-      else null
-    end
-  ) as PriceEpisodeProgFundIndMinCoInvest,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeSecondDisadvantagePayment' then Value
-      else null
-    end
-  ) as PriceEpisodeSecondDisadvantagePayment,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeSecondEmp1618Pay' then Value
-      else null
-    end
-  ) as PriceEpisodeSecondEmp1618Pay,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeSecondProv1618Pay' then Value
-      else null
-    end
-  ) as PriceEpisodeSecondProv1618Pay,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeSFAContribPct' then Value
-      else null
-    end
-  ) as PriceEpisodeSFAContribPct,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeESFAContribPct' then Value
-      else null
-    end
-  ) as PriceEpisodeESFAContribPct,
-  max(
-    case
-      AttributeName
-      when 'PriceEpisodeTotProgFunding' then Value
-      else null
-    end
-  ) as PriceEpisodeTotProgFunding
-from
-  (
-    select
-    UKPRN,
-      LearnRefNumber,
-      PriceEpisodeIdentifier,
-      AttributeName,
-      cast(substring([PeriodValue].[Period], 8, 2) as int) as [Period],
-      [PeriodValue].[Value]
-    from
-      Rulebase.AEC_ApprenticeshipPriceEpisode_PeriodisedValues unpivot (
-        [Value] for [Period] in (
-          Period_1,
-          Period_2,
-          Period_3,
-          Period_4,
-          Period_5,
-          Period_6,
-          Period_7,
-          Period_8,
-          Period_9,
-          Period_10,
-          Period_11,
-          Period_12
-        )
-      ) as PeriodValue
-  ) as UnrequiredAlias
-  join dbo.UKPRNForProcedures ufp
-	on ufp.UKPRN = UnrequiredAlias.UKPRN
-group by
-UnrequiredAlias.UKPRN,
-  LearnRefNumber,
-  PriceEpisodeIdentifier,
-  [Period]
+IF object_id(
+	'Rulebase.AEC_CopyHistory',
+	'p'
+) is not null begin drop procedure Rulebase.AEC_CopyHistory
+END
+GO
+
+CREATE PROCEDURE Rulebase.AEC_CopyHistory
+(  
+  @pCollectionReturnCode varchar(3)
+)
+AS 
+BEGIN
+	DELETE
+	FROM [ReferenceInput].[AppsEarningHistory_ApprenticeshipEarningsHistory]
+	WHERE [CollectionYear]='2021'
+		AND [CollectionReturnCode]=@pCollectionReturnCode
+		AND [UKPRN] in (select distinct UKPRN from Rulebase.AEC_HistoricEarningOutput)
+  
+	UPDATE [ReferenceInput].[AppsEarningHistory_ApprenticeshipEarningsHistory]
+	SET [LatestInYear]=0 WHERE [LatestInYear]=1
+		AND [CollectionYear]='2021'
+		AND [UKPRN] in (select distinct UKPRN from Rulebase.AEC_HistoricEarningOutput)
+
+
+	INSERT INTO [ReferenceInput].[AppsEarningHistory_ApprenticeshipEarningsHistory]
+		([AppIdentifier]
+		,[AppProgCompletedInTheYearInput]
+		,[CollectionYear]
+		,[CollectionReturnCode]
+		,[DaysInYear]
+		,[FworkCode]
+		,[HistoricEffectiveTNPStartDateInput]
+		,[HistoricEmpIdEndWithinYear]
+		,[HistoricEmpIdStartWithinYear]
+		,[HistoricLearner1618StartInput]
+		,[HistoricPMRAmount]
+		,[HistoricTNP1Input]
+		,[HistoricTNP2Input]
+		,[HistoricTNP3Input]
+		,[HistoricTNP4Input]
+		,[HistoricTotal1618UpliftPaymentsInTheYearInput]
+		,[HistoricVirtualTNP3EndOfTheYearInput]
+		,[HistoricVirtualTNP4EndOfTheYearInput]
+		,[HistoricLearnDelProgEarliestACT2DateInput]
+		,[LatestInYear]
+		,[LearnRefNumber]
+		,[ProgrammeStartDateIgnorePathway]
+		,[ProgrammeStartDateMatchPathway]
+		,[ProgType]
+		,[PwayCode]
+		,[STDCode]
+		,[TotalProgAimPaymentsInTheYear]
+		,[UptoEndDate]
+		,[UKPRN]
+		,[ULN])
+	SELECT 
+		[AppIdentifierOutput]
+		,[AppProgCompletedInTheYearOutput]
+		,'2021' as CollectionYear--2021
+		,@pCollectionReturnCode--
+		,[HistoricDaysInYearOutput]
+		,[HistoricFworkCodeOutput]
+		,[HistoricEffectiveTNPStartDateOutput]
+		,[HistoricEmpIdEndWithinYearOutput]
+		,[HistoricEmpIdStartWithinYearOutput]
+		,[HistoricLearner1618AtStartOutput]
+		,[HistoricPMRAmountOutput]
+		,[HistoricTNP1Output]
+		,[HistoricTNP2Output]
+		,[HistoricTNP3Output]
+		,[HistoricTNP4Output]
+		,[HistoricTotal1618UpliftPaymentsInTheYear]
+		,[HistoricVirtualTNP3EndofThisYearOutput]
+		,[HistoricVirtualTNP4EndofThisYearOutput]
+		,[HistoricLearnDelProgEarliestACT2DateOutput]
+		,1 as LatestInYear --true/1
+		,[LearnRefNumber]
+		,[HistoricProgrammeStartDateIgnorePathwayOutput]
+		,[HistoricProgrammeStartDateMatchPathwayOutput]
+		,[HistoricProgTypeOutput]
+		,[HistoricPwayCodeOutput]
+		,[HistoricSTDCodeOutput]
+		,[HistoricTotalProgAimPaymentsInTheYear]
+		,[HistoricUptoEndDateOutput]
+		,[UKPRN]
+		,[HistoricULNOutput]
+	FROM [Rulebase].[AEC_HistoricEarningOutput]
+END
+GO
